5dfbb6262a45ef33cf7a831656eb9985a98352b4
AlekseySamokhvalov <6560@mail.ru>
2023-08-13 14:43:31 +0300

    В классе Bicycle метод service() не переопределял.

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -2,9 +2,4 @@ public class Bicycle extends Vehicle {
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
-
-    @Override
-    public void service() {
-        super.service();
-    }
 }
